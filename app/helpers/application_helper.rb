3f6dc65d4718208f543532b5533f6e3d4671e424
shoutaro-u <overture21495@gmail.com>
2019-04-09 14:26:41 +0000

    static page finish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+  # ページごとにタイトルを返す。
+  def full_title(page_title = "")
+    base_title = "勤怠システムB2"
+    # page_titleが空なら真
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end

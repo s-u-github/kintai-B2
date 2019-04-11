e809d8334d1f0c13b5dab9f08be2f5508fe3321a
shoutaro-u <overture21495@gmail.com>
2019-04-11 06:51:57 +0000

    Finish users-signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger  # インスタンス変数を定義した直後にこのメソッドが実行される
   end
+  
+  def new
+    # 新規作成されたUserオブジェクトをインスタンス変数に代入する
+    @user = User.new
+  end
+  
+  def create
+    # Strong Parametersを用いる
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザーの新規作成に成功しました。"
+      # redirect_to user_url(@user)と同じ内容
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    # Strong Parameters 必須となるパラメータと許可されたパラメータを指定することができる。
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end

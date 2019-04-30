924cf5eb16a455d0df1591e7b9e96cf3ca69f549
shoutaro-u <overture21495@gmail.com>
2019-04-30 06:36:23 +0000

    faker japanese locale add

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module HelloApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Tokyo'
+    Faker::Config.locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

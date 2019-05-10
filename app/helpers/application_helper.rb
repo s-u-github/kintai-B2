8d904c6bd5ab032addc9a4686c8c2b2fc905a69b
shoutaro-u <overture21495@gmail.com>
2019-05-10 03:24:37 +0000

    header,show,レイアウト変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
     
   # ページごとにタイトルを返す。
   def full_title(page_title = "")
-    base_title = "勤怠システムB2"
+    base_title = "勤怠システム"
     # page_titleが空なら真
     if page_title.empty?
       base_title
